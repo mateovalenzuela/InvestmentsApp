9717fa5027e1e4470e0af7cae5294c0e9a1cbe92
Mateovalenzuela <mateo.valenzuela.dev@gmail.com>
2024-12-13 16:11:32 -0300

    feat: Cración de DTOs

---
+++ InvestmentsApp.Backend/DTOs/InsertUpdateTypeInvestmentDto.cs
@@ -0,0 +1,14 @@
+﻿using InvestmentsApp.Backend.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace InvestmentsApp.Backend.DTOs
+{
+    public class InsertUpdateTypeInvestmentDto
+    {
+        [Required]
+        [StringLength(30, MinimumLength = 4, ErrorMessage = "La descripcion debe tener entre 4 y 50 caracteres")]
+        public string Descripcion { get; set; }
+
+    }
+}
---
+++ InvestmentsApp.Backend/DTOs/InvestmentDto.cs
@@ -0,0 +1,28 @@
+﻿using InvestmentsApp.Backend.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace InvestmentsApp.Backend.DTOs
+{
+    public class InvestmentDto
+    {
+        public long Id { get; set; }
+
+        public string Tikcker { get; set; }
+
+        public string? Descripcion { get; set; }
+
+        public decimal ImporteInicial { get; set; }
+
+        public decimal ImporteFinal { get; set; }
+
+        public DateTime FechaEntrada { get; set; }
+
+        public DateTime FechaCierre { get; set; }
+
+        public DateTime FechaCreacion { get; set; }
+
+        public long IdTypeInvestment { get; set; }
+
+    }
+}
