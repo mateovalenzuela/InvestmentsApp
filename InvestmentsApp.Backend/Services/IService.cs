fbb09272113e4b7be9a5990e0606948a41f8f116
Mateovalenzuela <mateo.valenzuela.dev@gmail.com>
2024-12-14 13:32:54 -0300

    feat: Agregado de AutoMapper e Interfaz general de servicios

---
+++ InvestmentsApp.Backend/Services/IService.cs
@@ -0,0 +1,19 @@
+﻿namespace InvestmentsApp.Backend.Services
+{
+    public interface IService<Dto, insertDto, updateDto>
+    {
+        Dictionary<string,string> Errors { get; }
+
+        Task<IEnumerable<Dto>> GetAll();
+        Task<Dto?> GetById(long id);
+
+        Task<Dto> Add(insertDto dto);
+
+        Task<Dto?> Update(long id, updateDto dto);
+
+        Task<Dto?> Delete(long id);
+
+        bool Validate(insertDto dto);
+        bool Validate(long id, updateDto dto);
+    }
+}
---
+++ InvestmentsApp.Backend/Services/MapperProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using InvestmentsApp.Backend.DTOs.Investment;
+using InvestmentsApp.Backend.DTOs.TypeInvestment;
+using InvestmentsApp.Backend.Models;
+
+namespace InvestmentsApp.Backend.Services
+{
+    public class MapperProfile : Profile
+    {
+        public MapperProfile()
+        {
+            // TypeInvestment
+            CreateMap<InsertTypeInvestmentDto, TypeInvestment>();
+            CreateMap<TypeInvestment, TypeInvestmentDto>();
+
+            //Investment
+            CreateMap<InsertInvestmentDto, Investmetn>();
+            CreateMap<Investmetn, InvestmentDto>();
+        }
+    }
+}
